bb3620f32bcdd10d25c3ea2fe5ed6d0f6cec5e5f
YoungseoChoi23 <134666006+YoungseoChoi23@users.noreply.github.com>
2025-01-23 08:10:29 +0900

    Create wordCount.js

---
+++ wordCount.js
@@ -0,0 +1,14 @@
+function wordCount(text) {
+    const words = text.toLowerCase().split(" ");
+    const wordMap = {};
+    words.forEach(word => {
+        if (word in wordMap) {
+            wordMap[word] += 1;
+        } else {
+            wordMap[word] = 1;
+        }
+    });
+    return wordMap;
+}
+const text = "Hello world! Hello AI. Hello AI world!";
+console.log(wordCount(text));
